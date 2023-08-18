95081d4552c63cc0728d6321bf37abff111c7ff5
juanOchoa44 <140007421+juanOchoa44@users.noreply.github.com>
2023-08-18 17:34:00 -0500

    Diseño finalizado, versión 1.0 del signin
    Solamente hace falta agregar los links que llevan a otras páginas en los botones

---
+++ paginas/signin/rotacionImgs.js
@@ -0,0 +1,77 @@
+const imgsContenedor = document.querySelector(".left__imgs")
+const buttonLeft = document.querySelector("#flechaLeft")
+const buttonRight = document.querySelector("#flechaRight")
+const circles = document.querySelectorAll(".left__circles-circle")
+let numImg = 0
+let intervalImgsVar;
+let rutas = ["./images/example.jpg", "./images/evento.jpg", "./images/papas.jpg", "./images/balon.jpg", "./images/bolirana.jpg"]
+
+function colocadorImgs() {
+    let iterador = -1
+    rutas.forEach((ruta) => {
+        iterador ++
+
+        let img = document.createElement("img")
+        img.src = ruta
+        img.className = "left__imgs-img"
+        img.number = iterador
+        imgsContenedor.appendChild(img)
+    })
+
+}
+
+function cambioImg() {
+    rutas.forEach((element, i) => {
+        imgsContenedor.children[i].hidden = false
+        circles[i].style.backgroundColor = "transparent"
+
+        if (imgsContenedor.children[i].number != numImg) {
+            imgsContenedor.children[i].hidden = true
+        } else {
+            circles[i].style.backgroundColor = "#CB4335"
+        }
+    })
+}
+
+//---Funciones para los intervalos de rotar imágenes y detener cuando se da click en un botón durante 8 segundos
+const stopInterval = () => {
+    clearInterval(intervalImgsVar)
+    setTimeout(() => {
+        startInterval()
+    }, 8000)
+}
+
+const viewImgs = () => {
+    numImg ++
+    if (numImg >= rutas.length) {
+        numImg = 0
+    }
+    cambioImg()
+}
+
+const startInterval = () => {
+    intervalImgsVar = setInterval(viewImgs, 3000)
+}
+//------------
+
+buttonRight.addEventListener("click", () => {
+    numImg ++
+    if (numImg >= rutas.length) {
+        numImg = 0
+    }
+    cambioImg()
+    stopInterval()
+})
+
+buttonLeft.addEventListener("click", () => {
+    numImg --
+    if (numImg <= -1) {
+        numImg = rutas.length - 1
+    }
+    cambioImg()
+    stopInterval()
+})
+
+colocadorImgs()
+cambioImg()
+startInterval()
