52fee082418c1cbf07c24fc36349350bb18c8a0a
Miguel Angel Pulido Quinche <140007642+M1-gu3-lo@users.noreply.github.com>
2023-08-28 19:42:51 -0500

    Actualizando modelos

--- bolipola/core/models.py
+++ bolipola/core/models.py
@@ -57,8 +57,74 @@ class Output(models.Model):
         verbose_name_plural = 'Salidas'
         db_table = 'salida'
         ordering = ['id']
+      
+      
+class Calendar(models.Model):
+    date = models.DateTimeField(verbose_name='Fecha')
+    Availability = models.BooleanField(verbose_name='Disponibilidad', default=True)
 
-class Venta(models.Model):
+    def __str__(self):
+        return self.Availability
+    
+    class Meta:
+        verbose_name = 'Calendario'
+        verbose_name_plural = 'Calendarios'
+        db_table = 'calendario'
+        ordering = ['id']
+        
+  
+class Reservation(models.Model):
+    availability = models.BooleanField(verbose_name='Disponibilidad', default=True)
+    place = models.CharField(max_length=50, verbose_name='Lugar de la reserva')
+    type = models.CharField(max_length=50, verbose_name='Tipo de reserva')
+    start_time = models.DateTimeField(verbose_name='Hora de inicio')
+    end_time = models.DateTimeField(verbose_name='Hora de finalización')
+    cost = models.FloatField(verbose_name='Costo de la reserva')
+    Calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.availability
+    
+    class Meta:
+        verbose_name = 'Reserva'
+        verbose_name_plural = 'Reservas'
+        db_table = 'reserva'
+        ordering = ['id']
+
+class Event(models.Model):
+    type = models.CharField(max_length=50, verbose_name='Tipo de evento')
+    place = models.CharField(max_length=50, verbose_name='Lugar del evento')
+    date = models.DateTimeField(verbose_name='Fecha del evento')
+    cost = models.FloatField(verbose_name='Costo del evento')
+    guests = models.PositiveIntegerField(verbose_name='Cantidad de invitados')
+
+    def __str__(self):
+        return self.type
+    
+    class Meta:
+        verbose_name = 'Evento'
+        verbose_name_plural = 'Eventos'
+        db_table = 'evento'
+        ordering = ['id']
+
+class Tournament(models.Model):
+    number_teams = models.PositiveIntegerField(verbose_name='Cantidad de equipos')
+    registered_teams = models.PositiveIntegerField(verbose_name='Equipos registrados')
+    date = models.DateTimeField(verbose_name='Fecha del torneo')
+    prize_payment = models.FloatField(verbose_name='Pago de premio')
+    registration_cost = models.FloatField(verbose_name='Costo de inscripción')
+
+    def __str__(self):
+        return self.number_teams
+    
+    class Meta:
+        verbose_name = 'Torneo'
+        verbose_name_plural = 'Torneos'
+        db_table = 'torneo'
+        ordering = ['id']
+
+
+class Sale(models.Model):
     total_cost = models.FloatField(verbose_name='Costo total')
     payment_type = models.CharField(max_length=50, verbose_name='Tipo de pago')
     discount = models.BooleanField(verbose_name='Aplica descuento')
@@ -68,6 +134,9 @@ class Venta(models.Model):
     type = models.CharField(max_length=50, verbose_name='Tipo de venta')
     product_quantity = models.PositiveIntegerField(verbose_name='Cantidad de productos comprados')
     inventory = models.ManyToManyField(Inventory)
+    Event = models.ManyToManyField(Event, verbose_name='Venta_evento')
+    Reservation = models.ManyToManyField(Reservation, verbose_name='Venta_reserva')
+    Tournament = models.ManyToManyField(Tournament, verbose_name='Venta_torneo')
 
     def __str__(self):
         return self.payment_type
