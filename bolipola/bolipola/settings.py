7fd712c8af1d0af616fd2f0b85db7309136d3c5b
juanOchoa44 <140007421+juanOchoa44@users.noreply.github.com>
2023-09-04 14:03:04 -0500

    Se instala tema de admin y se actualiza

--- bolipola/bolipola/settings.py
+++ bolipola/bolipola/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_interface',
+    'colorfield',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core'
 ]
+
+X_FRAME_OPTIONS = "SAMEORIGIN"
+SILENCED_SYSTEM_CHECKS = ["security.W019"]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
