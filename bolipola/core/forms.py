f680f7b4103011cb3fff9db3f3dbad91957169d5
juanOchoa44 <140007421+juanOchoa44@users.noreply.github.com>
2023-09-21 08:28:46 -0500

    Confirmación ventas y crud torneo

--- bolipola/core/forms.py
+++ bolipola/core/forms.py
@@ -1,11 +1,10 @@
 from django import forms
-from .models import Team, Player, Sale, Product, Inventory, Category
+from .models import Team, Player, Sale, Product, Inventory, Category, TournamentTeam
 
 #Formulario de venta
 class SaleForm(forms.ModelForm):
 
     total_cost = forms.FloatField(
-        required=True,
         widget=forms.NumberInput(
             attrs={
                 'id':'totalCost',
@@ -28,7 +27,6 @@ class SaleForm(forms.ModelForm):
     )
 
     type = forms.CharField(
-        required=True,
         widget=forms.TextInput(
             attrs={
                 'id':'typeSale',
@@ -39,7 +37,6 @@ class SaleForm(forms.ModelForm):
     )
 
     product_quantity = forms.IntegerField(
-        required=True,
         widget=forms.NumberInput(
             attrs={
                 'readonly':'readonly',
@@ -111,6 +108,62 @@ class TeamForm(forms.ModelForm):
             'avatar',
         ]
 
+class TournamentTeamForm(forms.ModelForm):
+    goals_for = forms.IntegerField(
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                'class':'inf',
+            }
+        )
+    )
+
+    goals_against = forms.IntegerField(
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                'class':'inf',
+            }
+        )
+    )
+
+    games_tied = forms.IntegerField(
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                'class':'inf',
+            }
+        )
+    )
+
+    games_won = forms.IntegerField(
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                'class':'inf',
+            }
+        )
+    )
+
+    games_lost = forms.IntegerField(
+        required=True,
+        widget=forms.NumberInput(
+            attrs={
+                'class':'inf',
+            }
+        )
+    )
+
+    class Meta:
+        model = TournamentTeam
+        fields = [
+            'goals_for',
+            'goals_against',
+            'games_tied',
+            'games_won',
+            'games_lost',
+        ]
+
 #Formulario de jugador
 class PlayerForm(forms.ModelForm):
     name = forms.CharField(
@@ -172,6 +225,49 @@ class PlayerForm(forms.ModelForm):
             'age',
             'gender',
             'position',
+        ]
+
+class CardPlayerForm(forms.ModelForm):
+
+    yellow_card = forms.IntegerField(
+        required=True,
+        min_value=0,
+        max_value=3,
+        widget=forms.NumberInput(
+            attrs={
+                'class':'inf',
+            }
+        )
+    )
+
+    red_card = forms.IntegerField(
+        required=True,
+        min_value=0,
+        max_value=3,
+        widget=forms.NumberInput(
+            attrs={
+                'class':'inf',
+            }
+        )
+    )
+
+    blue_card = forms.IntegerField(
+        required=True,
+        min_value=0,
+        max_value=3,
+        widget=forms.NumberInput(
+            attrs={
+                'class':'inf',
+            }
+        )
+    )
+
+    class Meta:
+        model = Player
+        fields = [
+            'yellow_card',
+            'red_card',
+            'blue_card',
         ]
 
 #Formulario de producto
