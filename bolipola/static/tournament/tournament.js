adaf756854527e7e7de4df0c2a06d25e4f9d1ad8
juanOchoa44 <140007421+juanOchoa44@users.noreply.github.com>
2023-11-29 02:24:53 -0500

    Form para crear torneo

---
+++ bolipola/static/tournament/tournament.js
@@ -0,0 +1,34 @@
+const tourForm = document.querySelector(".tournament_form");
+const buttonTourForm = document.querySelector("#createTournament");
+const exitButtonForm = document.querySelector(".exit-create-container button");
+const firstInput = document.querySelector("#registerName");
+const main = document.querySelector(".main");
+const body = document.querySelector("body");
+const header = document.querySelector("header");
+let formActive = false;
+
+buttonTourForm.addEventListener("click", () => {
+    formActive = true;
+    tourForm.style.display = "block";
+    main.style.cssText = "opacity: 0.3;";
+    header.style.cssText = "opacity: 0.3;";
+    firstInput.focus();
+})
+
+exitButtonForm.addEventListener("click", () => {
+    formActive = false;
+    tourForm.style.display = "none";
+    main.style.cssText = "opacity: 1;";
+    header.style.cssText = "opacity: 1;";
+})
+
+//Se envía solo el formulario si se desea crear un torneo
+window.addEventListener("click", (e) => {
+    if (e.target.className == "buttonActive") {
+        return;
+    }
+
+    if (formActive) {
+        return e.preventDefault();
+    }
+})
