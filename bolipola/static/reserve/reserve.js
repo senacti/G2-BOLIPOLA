fe94d3533b17e230282f2c5f1581800e92a80b59
juanOchoa44 <140007421+juanOchoa44@users.noreply.github.com>
2023-12-01 14:25:38 -0500

    Solo minutos 30 y 00 en reserva

--- bolipola/static/reserve/reserve.js
+++ bolipola/static/reserve/reserve.js
@@ -1,22 +1,43 @@
-const form = document.querySelector(".form-calendar");
-const buttonDay = document.querySelector(".dispButton");
-const firstInput = document.querySelector(".input-container input");
-let active = false;
+const inputStartTime = document.querySelector("#hora-inicio");
+const inputEndTime = document.querySelector("#hora-fin");
 
-buttonDay.addEventListener("click", () => {
-    if (!active) {
-        form.style.display = "block";
-        firstInput.focus();
-        active = true;
-    } else {
-        form.style.display = "none";
-        active = false;
+function verifyMinutes(node) {
+    let minutesStr, hourStr, newTime;
+    let time = node.value;
+    let minutes = parseInt(time.split(":")[1])
+    let hour = parseInt(time.split(":"))
+
+    if (minutes != 30 || minutes != 0) {
+
+        if (minutes > 0 && minutes <= 15) {
+            minutes = 0;
+        } else if (minutes > 15 && minutes <= 30) {
+            minutes = 30;
+        } else if (minutes > 30 && minutes <= 45) {
+            minutes = 30;
+        } else {
+            minutes = 0;
+            hour++;
+        }
     }
+
+    minutesStr = String(minutes);
+    hourStr = String(hour);
+    if (minutes == 0) {
+        minutesStr = "00";
+    }
+    if (hour >= 0 && hour <= 9) {
+        hourStr = "0" + hourStr;
+    }
+
+    newTime = `${hourStr}:${minutesStr}`;
+    node.value = newTime;
+}
+
+inputStartTime.addEventListener("blur", () => {
+    verifyMinutes(inputStartTime);
 })
 
-window.addEventListener("click", (e) => {
-    if (e.target.attributes.group == undefined && active) {
-        form.style.display = "none";
-        active = false;
-    }
+inputEndTime.addEventListener("blur", () => {
+    verifyMinutes(inputEndTime);
 })
